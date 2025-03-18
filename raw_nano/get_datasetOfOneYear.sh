ac6b4d540e29f1a2feb5a29ab82b5485d432ee01
Xinlong <xliu5974@gmail.com>
2025-03-17 19:26:45 -0500

    Update json format

--- raw_nano/get_datasetOfOneYear.sh
+++
@@ -1,8 +0,0 @@
-year=$1
-./get_dataset.sh -y $year -d Data
-./get_dataset.sh -y $year -d MC_QCDJets 
-./get_dataset.sh -y $year -d MC_WZJets 
-./get_dataset.sh -y $year -d MC_TTBarJets 
-./get_dataset.sh -y $year -d MC_HiggsJets 
-./get_dataset.sh -y $year -d MC_DibosonJets 
-./get_dataset.sh -y $year -d MC_SingleTopJets 
